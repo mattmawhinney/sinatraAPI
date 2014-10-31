3920a576912c8a3dd00012030071c0750d3d78c3
Matt Mawhinney <matt.mawhinney@gmail.com>
2014-10-31 17:11:06 -0400

    modified hash syntax

--- server.rb
+++ server.rb
@@ -5,7 +5,7 @@ require 'json'
 # define a route that uses the helper
 get '/code' do
   content_type :json
-  { "code" =>  File.read('server.rb') }.to_json
+  { :code =>  File.read('server.rb') }.to_json
 end
 
 
